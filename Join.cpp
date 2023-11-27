af7f94cee4de8cfe43fdd5ff9a992c8c29aaf895
enesb <enesb@umich>
2023-11-26 22:35:01 -0500

    start functions

--- Join.cpp
+++ Join.cpp
@@ -10,8 +10,42 @@ using namespace std;
  */
 vector<Bucket> partition(Disk* disk, Mem* mem, pair<uint, uint> left_rel,
                          pair<uint, uint> right_rel) {
-	// TODO: implement partition phase
 	vector<Bucket> partitions(0, Bucket(disk)); // placeholder
+	// TODO: implement partition phase
+	for (uint32_t pId = left_rel.first; pId < left_rel.second; ++pId)
+	{
+		Page *pg = mem->mem_page(pId);
+		mem->loadFromDisk(disk, pId, pId);
+
+		for (uint32_t recId = 0; recId < pg->size(); ++recId)
+		{
+			Record r = pg->get_record(recId);
+			uint32_t hash = r.partition_hash() % (MEM_SIZE_IN_PAGE - 1);
+			partitions[hash].add_left_rel_page(pId);
+		}
+	}
+	// what memory page id do we flush?
+	// mem->flushToDisk(disk, )
+
+	for (uint32_t pId = right_rel.first; pId < right_rel.second; ++pId)
+	{
+		Page *pg = mem->mem_page(pId);
+		mem->loadFromDisk(disk, pId, pId);
+
+		for (uint32_t recId = 0; recId < pg->size(); ++recId)
+		{
+			Record r = pg->get_record(recId);
+			uint32_t hash = r.probe_hash() % (MEM_SIZE_IN_PAGE - 2);
+			partitions[hash].add_right_rel_page(pId);
+		}
+	}
+
+	// what memory page id do we flush?
+	// mem->flushToDisk(disk, )
+
+	
+
+	
 	return partitions;
 }
 
@@ -22,5 +56,10 @@ vector<Bucket> partition(Disk* disk, Mem* mem, pair<uint, uint> left_rel,
 vector<uint> probe(Disk* disk, Mem* mem, vector<Bucket>& partitions) {
 	// TODO: implement probe phase
 	vector<uint> disk_pages; // placeholder
+	for (uint32_t k = 0; k < MEM_SIZE_IN_PAGE; ++k)
+	{
+		Bucket& partition = partitions[k];
+	}
+	
 	return disk_pages;
 }
